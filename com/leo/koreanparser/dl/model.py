2891a63e4f7e8cc0ca2e842e3689bf40e56d2e43
junior bernard <junior.bernard-ext@alantaya.com>
2021-08-29 21:02:34 +0200

    Rajout de la normalisation et du dropout

--- com/leo/koreanparser/dl/model.py
+++ com/leo/koreanparser/dl/model.py
@@ -19,9 +19,9 @@ class MyModel(nn.Module):
         self.features2 = nn.Sequential(*layers[6:])
         self.classifier = nn.Sequential(nn.BatchNorm1d(512), nn.Linear(512, 512), nn.ReLU(), nn.Linear(512, 1))
         self.bb = nn.Sequential(nn.BatchNorm1d(512), nn.Linear(512, 256), nn.ReLU(),
-                                #nn.Dropout(),
+                                nn.Dropout(p=0.22),
                                 nn.Linear(256, 128), nn.ReLU(),
-                                #nn.BatchNorm1d(128),
+                                nn.BatchNorm1d(128),
                                 nn.Linear(128, 4), nn.ReLU())
 
     def forward(self, x):
