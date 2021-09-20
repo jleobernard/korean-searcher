f7d3f2462b7f6481565952e2984868c9d01b37d9
junior bernard <junior.bernard-ext@alantaya.com>
2021-09-20 22:05:55 +0200

    Modification de certains calculs. Seul reshape_target_boxes semble douteux.

--- com/leo/koreanparser/dl/model.py
+++ com/leo/koreanparser/dl/model.py
@@ -43,7 +43,6 @@ def get_model(eval: bool = False):
         model = model.train()
     return to_best_device(model)
 
-
 class ModelLoss:
 
     def __init__(self, weights: [float], width: float, height: float, cell_width_stride: float = 19, cell_height_stride: float = 13):
@@ -66,12 +65,13 @@ class ModelLoss:
         oneobj_hat = self.get_one_obj(preds)
         preds = preds.reshape(B * HxW, N)
         preds = torch.cat([preds[:, 1:3], preds[:, 4:]])
-        cell_with_corners = self.get_cell_with_corners(target_bbs, height=H, width=W)
-        oneobj_target = self.get_one_obj_target(target_bbs, cell_with_corners, height=H, width=W)
+        oneobj_target = self.get_one_obj_target(target_bbs, height=H, width=W)
         loss_cell_presence = F.binary_cross_entropy_with_logits(oneobj_hat, oneobj_target, reduction="mean")
         reshaped_target_boxes = self.reshape_target_boxes(target_bbs, height=H, width=W)
         loss_distance_to_corners = (((preds - reshaped_target_boxes) ** 2).sum(dim=1) * oneobj_hat).mean()
         return loss_presence, loss_cell_presence, loss_distance_to_corners
+
+
 
     def aggregate_losses(self, losses):
         my_loss = 0
@@ -100,21 +100,9 @@ class ModelLoss:
         preds.shape = # B, H x W, N
         :return tensor of shape B * HxW * 2 with one where the corner was predicted
         """
-        B,HxW, N = preds.shape
-        size_coord = B * HxW
-        oneobj = to_best_device(torch.zeros(size_coord * 2, requires_grad=False)) # x 2 because first ones are for ys and last ones for xs
-        y_preds = preds[:, :, 0]
-        _, indices = torch.max(y_preds, dim=1)
-        for i, val in enumerate(indices):
-            oneobj[i * HxW + val] = 1.
-        x_preds = preds[:, :, 3]
-        _, indices = torch.max(x_preds, dim=1)
-        for i, val in enumerate(indices):
-            oneobj[i * HxW + val + size_coord] = 1.
+        return torch.cat([preds[:, :, 0].flatten(), preds[:, :, 3].flatten()])
 
-        return oneobj
-
-    def get_one_obj_target(self, target_bbs, corners, height: int, width: int):
+    def get_one_obj_target(self, target_bbs, height: int, width: int):
         """
         Transforme les target boxes en un grand one-hot vector de longueur B x H x W x 2.
         Il y a un 1 si la case est censée contenir un coin. Le coin en haut à gauche est contenu dans les B x H x W
@@ -125,10 +113,11 @@ class ModelLoss:
         :param width: le nombde de cellules par image en largeur
         :return: vecteur de taille B x H x W x 2
         """
+        corners = self.get_cell_with_corners(target_bbs, height=height, width=width)
         HxW = height * width
         B = target_bbs.shape[0]
         size_coord = B * HxW
-        oneobj = to_best_device(torch.zeros(size_coord * 2, requires_grad=False))
+        oneobj = torch.zeros(size_coord * 2, requires_grad=False, device=target_bbs.device)
         for i, val in enumerate(corners[:, 0]):
             oneobj[i * HxW + int(val)] = 1.
         for i, val in enumerate(corners[:, 1]):
@@ -146,15 +135,16 @@ class ModelLoss:
         :param width: Nombre de cellules par largeur
         :return:
         """
+        # TODO peut-être douteux
         B, _ = target_bbs.shape
         HxW = height * width
         targets = torch.cat([coord.expand(HxW, -1) for coord in torch.cat([target_bbs[:, 0:2], target_bbs[:, 2:]])])
-        template_origin = to_best_device(torch.zeros((HxW, 2), requires_grad=False))
+        template_origin = torch.zeros((HxW, 2), requires_grad=False, device=target_bbs.device)
         idx = 0
         for i in range(height):
             ii = i / height
             for j in range(width):
-                template_origin[idx] = to_best_device(torch.tensor([ii, j / width], requires_grad=False))
+                template_origin[idx] = torch.tensor([ii, j / width], requires_grad=False, device=target_bbs.device)
                 idx += 1
         template_origin = template_origin.repeat(B * 2, 1)
         targets = (targets - template_origin) * to_best_device(torch.tensor([height, width], requires_grad=False))
@@ -163,8 +153,8 @@ class ModelLoss:
 
 
     def get_cell_with_corners(self, target_bbs, height, width):
-        new_tbs = torch.floor(
-            target_bbs * torch.tensor([height, width, height, width], requires_grad=False, device=target_bbs.device))
+        dims = torch.tensor([height, width, height, width], requires_grad=False, device=target_bbs.device)
+        new_tbs = torch.floor(target_bbs * dims)
         corners = torch.cat([
             (new_tbs[:, 0] * width + new_tbs[:, 1]).unsqueeze(0),
             (new_tbs[:, 2] * width + new_tbs[:, 3]).unsqueeze(0)
