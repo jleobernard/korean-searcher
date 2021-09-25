776fd00ead010c81b06afd2390cce193398f46eb
junior bernard <junior.bernard-ext@alantaya.com>
2021-09-25 14:41:37 +0200

    Correction nom distant

--- scripts/local/dl-model.sh
+++ scripts/local/dl-model.sh
@@ -13,8 +13,8 @@ fi
 echo "Réalisation de la copie de sauvegarde..."
 MYDATE="`date +%Y%m%dH%H%M`"
 distant_history_dir="$distant_model_dir/history"
-distant_file="$distant_model_dir/best.pt"
+distant_file="$distant_model_dir/model.pt"
 read -p "Commentaire (une seule ligne) ? " comment
 comment=${comment:-"na"}
-ssh $distant_server "mkdir -p $distant_history_dir && touch $distant_history_dir/comments.txt && if test -f $distant_file; then cp $distant_file $distant_history_dir/best.pt-$MYDATE.old; echo 'best.pt-$MYDATE.old=$comment' >> $distant_history_dir/comments.txt; else echo 'Aucun fichier à sauvegarder'; fi"
-scp "$DIR/../../com/leo/koreanparser/dl/data/models/best.pt" $distant_server:/opt/data/models/korean-subs/best.pt
+ssh $distant_server "mkdir -p $distant_history_dir && touch $distant_history_dir/comments.txt && if test -f $distant_file; then cp $distant_file $distant_history_dir/model.pt-$MYDATE.old; echo 'model.pt-$MYDATE.old=$comment' >> $distant_history_dir/comments.txt; else echo 'Aucun fichier à sauvegarder'; fi"
+scp "$DIR/../../com/leo/koreanparser/dl/data/models/best.pt" $distant_server:$distant_file
