275600d0cedaa5d7d13d56727c8db21247c68b8e
junior bernard <junior.bernard-ext@alantaya.com>
2021-09-25 14:38:54 +0200

    Téléchargement du modèle

---
+++ scripts/local/dl-model.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+if test -f "$DIR/.conf"; then
+  source "$DIR/.conf"
+elif test -f "$DIR/../.conf"; then
+  source "$DIR/../.conf"
+else
+  echo "Aucun fichier de configuration $DIR/.conf ou $DIR/../.conf trouvé"
+  exit -1
+fi
+
+echo "Réalisation de la copie de sauvegarde..."
+MYDATE="`date +%Y%m%dH%H%M`"
+distant_history_dir="$distant_model_dir/history"
+distant_file="$distant_model_dir/best.pt"
+read -p "Commentaire (une seule ligne) ? " comment
+comment=${comment:-"na"}
+ssh $distant_server "mkdir -p $distant_history_dir && touch $distant_history_dir/comments.txt && if test -f $distant_file; then cp $distant_file $distant_history_dir/best.pt-$MYDATE.old; echo 'best.pt-$MYDATE.old=$comment' >> $distant_history_dir/comments.txt; else echo 'Aucun fichier à sauvegarder'; fi"
+scp "$DIR/../../com/leo/koreanparser/dl/data/models/best.pt" $distant_server:/opt/data/models/korean-subs/best.pt
