45fb6fbd949f7efc32bd8a24607af94513690d5f
junior bernard <junior.bernard-ext@alantaya.com>
2021-09-26 09:19:49 +0200

    Renommage

--- com/leo/koreanparser/main.py
+++ com/leo/koreanparser/main.py
@@ -38,9 +38,9 @@ class Handler(FileSystemEventHandler):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Démarrage du pipeline d'extraction de sous-titres")
-    parser.add_argument('--conf', dest='conf', help='Path to conf', required=True)
+    parser.add_argument('--conf', dest='conf_path', help='Path to conf', required=True)
     args = parser.parse_args()
-    load_dotenv(args.conf)
+    load_dotenv(args.conf_path)
     income_dir = os.getenv("income_dir")
     model_dir = os.getenv("model_dir")
     watcher = IncomingVideoFileWatcher(income_dir)
