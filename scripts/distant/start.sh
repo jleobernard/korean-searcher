f902d2578a7a6b7aa4f5b80364ccd9e2c03bcb6e
junior bernard <junior.bernard-ext@alantaya.com>
2021-09-26 08:50:24 +0200

    WIP

--- scripts/distant/start.sh
+++ scripts/distant/start.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 set -e
 DIR=$(realpath "$(dirname "$0")")
-source $DIR/../../.venv/bin/activate
-python3 com/leo/koreanparser/main.py --conf $DIR/.env
+cd $DIR/../..
+root_dir=$(pwd)
+source $root_dir/.venv/bin/activate
+python3 com/leo/koreanparser/main.py --conf $root_dir/.env
