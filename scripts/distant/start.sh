af77adc2f4d5740ce706aaa9b2cbc3b322f5db8b
junior bernard <junior.bernard-ext@alantaya.com>
2021-09-26 08:19:40 +0200

    Retrait de quelques dépendences inutilisées (il doit en rester)

--- scripts/distant/start.sh
+++ scripts/distant/start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+DIR=$(realpath "$(dirname "$0")")
 source $DIR/../../.venv/bin/activate
 python3 com/leo/koreanparser/main.py --conf $DIR/.env
