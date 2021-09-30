feb1a5682fbf23df1d561f3f8edeb4430bb2f5e6
junior bernard <junior.bernard-ext@alantaya.com>
2021-09-30 22:24:30 +0200

    Ensure dir

--- com/leo/koreanparser/main.py
+++ com/leo/koreanparser/main.py
@@ -46,11 +46,8 @@ class Handler(FileSystemEventHandler):
 
     def ensure_dir(self, file_path):
         directory = os.path.dirname(file_path)
-        if os.path.exists(directory):
-            print(f"Directory {file_path} already exists")
-        else:
-            print(f"Creating dir {directory}")
-            os.makedirs(directory)
+        print(f"Creating dir {directory}")
+        os.makedirs(directory, exist_ok=True)
 
     def on_any_event(self, event: FileSystemEvent):
         ready_file_path = event.src_path
