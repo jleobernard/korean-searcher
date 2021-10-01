de10bfa1f39c735434a6bc6d793ad064b432c8af
junior bernard <junior.bernard-ext@alantaya.com>
2021-10-01 13:20:05 +0200

    Problème d'env

--- scripts/distant/start.sh
+++ scripts/distant/start.sh
@@ -4,4 +4,5 @@ DIR=$(realpath "$(dirname "$0")")
 cd $DIR/../..
 root_dir=$(pwd)
 source $root_dir/.venv/bin/activate
+export PYTHONPATH=$root_dir
 python3 com/leo/koreanparser/main.py --conf $DIR/.env
