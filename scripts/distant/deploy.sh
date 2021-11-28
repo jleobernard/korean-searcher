f9947e9e8151cc662895f81690be6902355ec1ae
junior bernard <junior.bernard-ext@alantaya.com>
2021-11-28 18:07:03 +0100

    Changement du nom du serveur

--- scripts/distant/deploy.sh
+++ scripts/distant/deploy.sh
@@ -3,5 +3,5 @@ set -e
 DIR=$(realpath "$(dirname "$0")")
 cd $DIR/../..
 root_dir=$(pwd)
-rsync -vrza $root_dir/server/* ks-leo-noport:/opt/containers/kosubs/docker/app
-ssh ks-leo-noport "cd /opt/containers/kosubs && mv docker/app/requirements.txt docker && docker-compose up -d --build"
+rsync -vrza $root_dir/server/* mandela:/opt/containers/kosubs/docker/app
+ssh mandela "cd /opt/containers/kosubs && mv docker/app/requirements.txt docker && docker-compose up -d --build"
