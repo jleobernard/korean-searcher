97f6ea33228b5eebe0c1749300ed1406935b02c7
junior bernard <junior.bernard-ext@alantaya.com>
2021-12-27 17:08:56 +0100

    Correction prise en compte du fichier de conf (one conf to rull them all)

--- scripts/distant/analyze.sh
+++ scripts/distant/analyze.sh
@@ -4,14 +4,16 @@ set -e
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
+my_env=""
 if test -f "$DIR/.conf"; then
-  source "$DIR/.conf"
+  my_env="$DIR/.conf"
 elif test -f "$DIR/../.conf"; then
-  source "$DIR/../.conf"
+  my_env="$DIR/../.conf"
 else
   echo "Aucun fichier de configuration $DIR/.conf ou $DIR/../.conf trouvé"
   exit -1
 fi
+source "$my_env"
 
 source $project_path/.venv/bin/activate
 export PYTHONPATH=$project_path
@@ -23,7 +25,7 @@ function process_file() {
     read -p "Analyser le fichier $file ? [Y/n] " answer
     answer=${answer:-"y"}
     if [[ "$answer" = "y" ]]; then
-      python3 $project_path/com/leo/koreanparser/main.py --conf $project_path/scripts/distant/.env --file "$file"
+      python3 $project_path/com/leo/koreanparser/main.py --conf $my_env --file "$file"
       if [[ $? -eq 0 ]]; then
         echo "Anaylse du fichier $file terminée"
         read -p "Recharger les sous-titres du serveur ? [Y/n] " answer
@@ -46,7 +48,7 @@ function process_file() {
       echo "Fichier ignoré ($file)"
     fi
 }
-cd $local_income_dir
+cd $income_dir
 files=$(ls . | grep '.*.webm')
 for file in $files
 do
