e8a39dc77ea4ac7c5f08f46931ac267853aac198
Junior Léo Bernard <jleobernard@gmail.com>
2022-09-05 14:24:02 +0200

    Denormalize image

--- com/leo/koreanparser/dl/utils/image_helper.py
+++ com/leo/koreanparser/dl/utils/image_helper.py
@@ -1,7 +1,25 @@
 import numpy as np
+import torchvision
+import torch
 
 
 def normalize_imagenet(im):
     """Normalizes images with Imagenet stats."""
     imagenet_stats = np.array([[0.485, 0.456, 0.406], [0.229, 0.224, 0.225]])
     return (im - imagenet_stats[0])/imagenet_stats[1]
+
+
+class NormalizeInverse(torchvision.transforms.Normalize):
+    """
+    Undoes the normalization and returns the reconstructed images in the input domain.
+    """
+
+    def __init__(self, mean, std):
+        mean = torch.as_tensor(mean)
+        std = torch.as_tensor(std)
+        std_inv = 1 / (std + 1e-7)
+        mean_inv = -mean * std_inv
+        super().__init__(mean=mean_inv, std=std_inv)
+
+    def __call__(self, tensor):
+        return super().__call__(tensor.clone())
